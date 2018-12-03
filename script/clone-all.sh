73008f497fa16c84536c831ef2d612d09f560f03
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-02 20:54:03 -0500

    grep

--- script/clone-all.sh
+++ script/clone-all.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+set -e
+
 while read line
 do
-  git clone git@github.com:$line.git
+  ORG=`dirname $line` 
+  REPO=`basename $line`
+  git clone git@github.com:typedclojure/$REPO.git
+  cd $REPO
+  git remote rm origin
+  git remote add typedclojure git@github.com:typedclojure/$REPO.git
+  git branch --set-upstream-to typedclojure/master
+  if [ "$ORG" == "clojure" ]; then
+    git remote add clojure git@github.com:clojure/$REPO.git
+  fi
+  cd ..
 done < projects
