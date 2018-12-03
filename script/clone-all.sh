2e300c43aa3e6026770e94777baf27e348d1e5f0
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-02 21:06:22 -0500

    set origin

--- script/clone-all.sh
+++ script/clone-all.sh
@@ -6,10 +6,8 @@ while read line
 do
   ORG=`dirname $line` 
   REPO=`basename $line`
-  git clone git@github.com:typedclojure/$REPO.git
+  git clone git@github.com:typedclojure/$REPO.git --origin typedclojure
   cd $REPO
-  git remote rm origin
-  git remote add typedclojure git@github.com:typedclojure/$REPO.git
   git branch --set-upstream-to typedclojure/master
   if [ "$ORG" == "clojure" ]; then
     git remote add clojure git@github.com:clojure/$REPO.git
